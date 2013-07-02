20fb2afb6f7c2da39c31c436c3bdec76136f1583
Jasal Vadgama <jasal@blacklabelcreative.com>
2013-07-02 23:22:41 +0100

    adding equal height js. amending portfolio image style.

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,32 @@
 var BLC = BLC || {};
 
 BLC.app = (function() {
-	// PRIVATE VARIABLES
+	var
+		// PRIVATE VARIABLES
+		largeScreenWidth = 959,
+		windowWidth,
+		$content = $("#content"),
+		$sidebar = $("#sidebar"),
 
-	// PRIVATE FUNCTIONS
+		// PRIVATE FUNCTIONS
+		equaliseColumns = function() {
+			// make sure column heights are equal for large screens
+
+			// if it's a large screen
+			if (windowWidth >= largeScreenWidth) {
+				// if sidebar is taller than the content
+				if ($sidebar.outerHeight() > $content.outerHeight()) {
+					$content.css({
+						height: $sidebar.outerHeight()
+					});
+				}
+			} else {
+				// remove fixed heights
+				$content.css({
+					height: "auto"
+				});
+			}
+		};
 
 	// PUBLIC METHODS
 	return {
@@ -19,9 +42,24 @@ BLC.app = (function() {
 
 				window.open($(this).attr("href"));
 			});
+
+			// init the resize listener
+			$(window).on("resize", function() {
+				BLC.app.resizeListener();
+			});
 		},
 		pageInit: function() {
 			// page load
+
+			// set page vars
+			BLC.app.resizeListener();
+		},
+		resizeListener: function() {
+			// update vars on resize
+			windowWidth = $(window).width();
+
+			// resize the content
+			equaliseColumns();
 		}
 	};
 }());
