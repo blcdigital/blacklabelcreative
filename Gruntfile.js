bb2f48412f1f524d101d1597b0bb847415b1d1b4
Jasal Vadgama <jasal@blacklabelcreative.com>
2016-01-10 18:52:24 +0000

    fixing landscape small screen layout. fixing deploy build process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,6 +155,6 @@ module.exports = function(grunt) {
     // // Default task(s)
     // grunt.registerTask('test', ['jshint']);
     grunt.registerTask('build', ['clean', 'copy', 'sass'/*, 'browserify'*/]);
-    grunt.registerTask('deploy', ['test', 'jekyll:build', 'build', 'cssmin', /*'uglify', */'cache-busting']);
+    grunt.registerTask('deploy', [/*'test', */'jekyll:build', 'build', 'cssmin', /*'uglify', */'cache-busting']);
     grunt.registerTask('default', ['build', 'concurrent']);
 };
